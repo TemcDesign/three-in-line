a6ce11e2ef54f044c1811ca4b77f493ce635c791
ThomasMujica <temc.business@gmail.com>
2024-08-31 10:33:42 -0400

    Comienzo de subida a produccion

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./header";
+import "./style.css";
+import Target from "./target";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <Target />
+    </>
+  );
+}
